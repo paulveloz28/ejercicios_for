501471036079953e7fb42d2975bbe12fc1579304
paulveloz28 <paulveloz28@gmail.com>
2026-04-23 15:00:54 -0500

    feat: add the listaImpares function

--- ejercicio.js
+++ ejercicio.js
@@ -11,6 +11,8 @@ function ejecutar(numEjercicio){
         listarNumerosReversa();
     }else if(numEjercicio==3){
         listarPares();
+    }else if(numEjercicio==4){
+        listarImpares();
     }
 }
 
@@ -26,3 +28,8 @@ function listarPares(){
     }
 }
 
+function listarImpares(){
+    for(let i=1;i<=7;i+=2){
+        console.log(i);
+    }
+}
